127bbf076555eb240dba2858d83f71eda5439f46
Brian Warner <warner@lothar.com>
2011-11-15 17:53:36 -0800

    change pyflakes-hushing code, temporarily remove __version__ handling

--- src/ed25519/__init__.py
+++ src/ed25519/__init__.py
@@ -1,9 +1,8 @@
-from ._version import __version__
 from .keys import (BadSignatureError, BadPrefixError,
                    create_keypair, SigningKey, VerifyingKey,
                    remove_prefix, to_ascii, from_ascii)
 
-hush_pyflakes=(__version__, BadSignatureError, BadPrefixError,
-               create_keypair, SigningKey, VerifyingKey,
-               remove_prefix, to_ascii, from_ascii)
-del hush_pyflakes
+(BadSignatureError, BadPrefixError,
+ create_keypair, SigningKey, VerifyingKey,
+ remove_prefix, to_ascii, from_ascii) # hush pyflakes
+
