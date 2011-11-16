09dd575ba3ae19715e315f26825b0f97a61c9e83
Brian Warner <warner@lothar.com>
2011-11-16 15:44:47 -0800

    add 'setup.py test_kat'

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 
 import sys, os
 from distutils.core import setup, Extension, Command
+from distutils.util import get_platform
 import versioneer
 versioneer.versionfile_source = "src/ed25519/_version.py"
 versioneer.versionfile_build = "ed25519/_version.py"
@@ -28,7 +29,6 @@ sources.extend(["src/ed25519-supercop-ref/"+s
 m = Extension("ed25519/_ed25519",
               include_dirs=["src/ed25519-supercop-ref"], sources=sources)
 
-from distutils.util import get_platform
 class Test(Command):
     description = "run tests"
     user_options = []
@@ -36,21 +36,34 @@ class Test(Command):
         pass
     def finalize_options(self):
         pass
-    def run(self):
+    def setup_path(self):
         # copied from distutils/command/build.py
         self.plat_name = get_platform()
         plat_specifier = ".%s-%s" % (self.plat_name, sys.version[0:3])
         self.build_lib = os.path.join("build", "lib"+plat_specifier)
         sys.path.insert(0, self.build_lib)
-        import ed25519.test
+    def run(self):
+        self.setup_path()
         import unittest
-        test = unittest.defaultTestLoader.loadTestsFromModule(ed25519.test)
+        test = unittest.defaultTestLoader.loadTestsFromName("ed25519.test")
+        runner = unittest.TextTestRunner(verbosity=2)
+        result = runner.run(test)
+        sys.exit(not result.wasSuccessful())
+
+class KnownAnswerTest(Test):
+    description = "run known-answer-tests"
+    def run(self):
+        self.setup_path()
+        import unittest
+        test = unittest.defaultTestLoader.loadTestsFromName("test-kat")
         runner = unittest.TextTestRunner(verbosity=2)
         result = runner.run(test)
         sys.exit(not result.wasSuccessful())
 
 commands = versioneer.get_cmdclass().copy()
 commands["test"] = Test
+commands["test_kat"] = KnownAnswerTest
+
 setup(name="ed25519",
       version=versioneer.get_version(),
       description="Ed25519 public-key signatures",
