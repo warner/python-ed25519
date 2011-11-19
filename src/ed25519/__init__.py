b89e90d5f60244d65a7bc69d1863d64f4dde4f4e
Brian Warner <warner@lothar.com>
2011-11-19 12:01:24 -0800

    update to versioneer-0.3

--- src/ed25519/__init__.py
+++ src/ed25519/__init__.py
@@ -6,5 +6,6 @@ from .keys import (BadSignatureError, BadPrefixError,
  create_keypair, SigningKey, VerifyingKey,
  remove_prefix, to_ascii, from_ascii) # hush pyflakes
 
-from ._version import __version__
-__version__ # hush pyflakes
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
