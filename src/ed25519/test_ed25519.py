ece701fdab5467f7ca022456cd79bd18e4d5a0cc
Brian Warner <warner@lothar.com>
2015-07-03 11:14:46 -0700

    fix tests for pypy3 compatibility

--- src/ed25519/test_ed25519.py
+++ src/ed25519/test_ed25519.py
@@ -54,8 +54,8 @@ class Basic(unittest.TestCase):
         sk_s = b"\x00" * 32 # usually urandom(32)
         vk_s, skvk_s = raw.publickey(sk_s)
         self.failUnlessEqual(len(vk_s), 32)
-        exp_vks = unhexlify("3b6a27bcceb6a42d62a3a8d02a6f0d73"
-                            "653215771de243a63ac048a18b59da29")
+        exp_vks = unhexlify(b"3b6a27bcceb6a42d62a3a8d02a6f0d73"
+                            b"653215771de243a63ac048a18b59da29")
         self.failUnlessEqual(vk_s, exp_vks)
         self.failUnlessEqual(skvk_s[:32], sk_s)
         self.failUnlessEqual(skvk_s[32:], vk_s)
@@ -63,10 +63,10 @@ class Basic(unittest.TestCase):
         msg_and_sig = raw.sign(msg, skvk_s)
         sig = msg_and_sig[:-len(msg)]
         self.failUnlessEqual(len(sig), 64)
-        exp_sig = unhexlify("b0b47780f096ae60bfff8d8e7b19c36b"
-                            "321ae6e69cca972f2ff987ef30f20d29"
-                            "774b53bae404485c4391ddf1b3f37aaa"
-                            "8a9747f984eb0884e8aa533386e73305")
+        exp_sig = unhexlify(b"b0b47780f096ae60bfff8d8e7b19c36b"
+                            b"321ae6e69cca972f2ff987ef30f20d29"
+                            b"774b53bae404485c4391ddf1b3f37aaa"
+                            b"8a9747f984eb0884e8aa533386e73305")
         self.failUnlessEqual(sig, exp_sig)
         ret = raw.open(sig+msg, vk_s) # don't raise exception
         self.failUnlessEqual(ret, msg)
@@ -112,8 +112,8 @@ class Basic(unittest.TestCase):
 
 
     def test_publickey(self):
-        seed = unhexlify("4ba96b0b5303328c7405220598a587c4"
-                         "acb06ed9a9601d149f85400195f1ec3d")
+        seed = unhexlify(b"4ba96b0b5303328c7405220598a587c4"
+                         b"acb06ed9a9601d149f85400195f1ec3d")
         sk = ed25519.SigningKey(seed)
         self.failUnlessEqual(hexlify(sk.to_bytes()),
                              (b"4ba96b0b5303328c7405220598a587c4"
@@ -129,46 +129,46 @@ class Basic(unittest.TestCase):
         self.failUnlessEqual(sk, sk2)
 
     def test_OOP(self):
-        sk_s = unhexlify("4ba96b0b5303328c7405220598a587c4"
-                         "acb06ed9a9601d149f85400195f1ec3d"
-                         "a66d161e090652b054740748f059f92a"
-                         "5b731f1c27b05571f6d942e4f8b7b264")
+        sk_s = unhexlify(b"4ba96b0b5303328c7405220598a587c4"
+                         b"acb06ed9a9601d149f85400195f1ec3d"
+                         b"a66d161e090652b054740748f059f92a"
+                         b"5b731f1c27b05571f6d942e4f8b7b264")
         sk = ed25519.SigningKey(sk_s)
         self.failUnlessEqual(len(sk.to_bytes()), 64)
         self.failUnlessEqual(sk.to_bytes(), sk_s)
 
-        sk2_seed = unhexlify("4ba96b0b5303328c7405220598a587c4"
-                             "acb06ed9a9601d149f85400195f1ec3d")
+        sk2_seed = unhexlify(b"4ba96b0b5303328c7405220598a587c4"
+                             b"acb06ed9a9601d149f85400195f1ec3d")
         sk2 = ed25519.SigningKey(sk2_seed)
         self.failUnlessEqual(sk2.to_bytes(), sk.to_bytes())
 
         vk = sk.get_verifying_key()
         self.failUnlessEqual(len(vk.to_bytes()), 32)
-        exp_vks = unhexlify("a66d161e090652b054740748f059f92a"
-                            "5b731f1c27b05571f6d942e4f8b7b264")
+        exp_vks = unhexlify(b"a66d161e090652b054740748f059f92a"
+                            b"5b731f1c27b05571f6d942e4f8b7b264")
         self.failUnlessEqual(vk.to_bytes(), exp_vks)
         self.failUnlessEqual(ed25519.VerifyingKey(vk.to_bytes()), vk)
         msg = b"hello world"
         sig = sk.sign(msg)
         self.failUnlessEqual(len(sig), 64)
-        exp_sig = unhexlify("6eaffe94f2972b35158b6aaa9b69c1da"
-                            "97f0896aca29c41b1dd7b32e6c9e2ff6"
-                            "76fc8d8b034709cdcc37d8aeb86bebfb"
-                            "173ace3c319e211ea1d7e8d8884c1808")
+        exp_sig = unhexlify(b"6eaffe94f2972b35158b6aaa9b69c1da"
+                            b"97f0896aca29c41b1dd7b32e6c9e2ff6"
+                            b"76fc8d8b034709cdcc37d8aeb86bebfb"
+                            b"173ace3c319e211ea1d7e8d8884c1808")
         self.failUnlessEqual(sig, exp_sig)
         self.failUnlessEqual(vk.verify(sig, msg), None) # also, don't throw
         self.failUnlessRaises(ed25519.BadSignatureError,
                               vk.verify, sig, msg+b".. NOT!")
 
     def test_object_identity(self):
-        sk1_s = unhexlify("ef32972ae3f1252a5aa1395347ea008c"
-                          "bd2fed0773a4ea45e2d2d06c8cf8fbd4"
-                          "c024601a9c5b854fb100ff3116cf4f22"
-                          "a311565f027391cb49d3bbe11c44399d")
-        sk2_s = unhexlify("3d550c158900b4c2922b6656d2f80572"
-                          "89de4ee65043745179685ae7d29b944d"
-                          "672b8a2cb23f9e75e1d46ce249cd9c04"
-                          "68f816f1c734a102822b60e18b41eacd")
+        sk1_s = unhexlify(b"ef32972ae3f1252a5aa1395347ea008c"
+                          b"bd2fed0773a4ea45e2d2d06c8cf8fbd4"
+                          b"c024601a9c5b854fb100ff3116cf4f22"
+                          b"a311565f027391cb49d3bbe11c44399d")
+        sk2_s = unhexlify(b"3d550c158900b4c2922b6656d2f80572"
+                          b"89de4ee65043745179685ae7d29b944d"
+                          b"672b8a2cb23f9e75e1d46ce249cd9c04"
+                          b"68f816f1c734a102822b60e18b41eacd")
         sk1a = ed25519.SigningKey(sk1_s)
         sk1b = ed25519.SigningKey(sk1_s)
         vk1a = sk1a.get_verifying_key()
